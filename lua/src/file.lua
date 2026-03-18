641b9283fc2c90ac1be8ad3927d741dec66ebb47
Leon Si <leon@leonsilicon.com>
2026-03-18 08:50:58 -0400

    fix

--- lua/src/file.lua
+++ lua/src/file.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local file = require("pl.file")
+
 -- escape Lua pattern characters so markers are treated literally
 local function escape_pattern(s)
   return s:gsub("([^%w])", "%%%1")
@@ -10,7 +12,7 @@ function M.upsert_block(path, new_content, start_marker, end_marker)
   start_marker = start_marker or "# START"
   end_marker = end_marker or "# END"
 
-  local existing = M.read(path) or ""
+  local existing = file.read(path) or ""
 
   local block = start_marker .. "\n"
     .. new_content .. "\n"
