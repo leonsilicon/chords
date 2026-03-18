38cb1fbcda165b8f9cbf9927c8d4e19d8814466e
Leon Si <leon@leonsilicon.com>
2026-03-18 17:43:26 -0400

    fix

--- lua/src/maketheweb_helpers.lua
+++ lua/src/maketheweb_helpers.lua
@@ -5,10 +5,10 @@ local json = require("lunajson")
 local plist = require("plist")
 
 function M.make_shortcut(filepath)
-  local plist = plist.parse_file(filepath)
+  local data = plist.parse_file(filepath)
 
   return function (property)
-    local value = json.decode(plist[1][property])
+    local value = json.decode(data[1][property])
     local keys = mac_keycode.carbon_modifiers_to_strings(payload.carbonModifiers)
     table.insert(keys, mac_keycode.keycode_to_string(payload.carbonKey))
     tap(table.concat(keys, "+"))
