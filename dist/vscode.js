273ae14511b1031b03cfb456d4db6d28c8928a1f
Leon Si <leon@leonsilicon.com>
2026-03-20 20:26:18 -0400

    build

---
+++ dist/vscode.js
@@ -0,0 +1,95 @@
+var __create = Object.create;
+var __getProtoOf = Object.getPrototypeOf;
+var __defProp = Object.defineProperty;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+function __accessProp(key) {
+  return this[key];
+}
+var __toESMCache_node;
+var __toESMCache_esm;
+var __toESM = (mod, isNodeMode, target) => {
+  var canCache = mod != null && typeof mod === "object";
+  if (canCache) {
+    var cache = isNodeMode ? __toESMCache_node ??= new WeakMap : __toESMCache_esm ??= new WeakMap;
+    var cached = cache.get(mod);
+    if (cached)
+      return cached;
+  }
+  target = mod != null ? __create(__getProtoOf(mod)) : {};
+  const to = isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target;
+  for (let key of __getOwnPropNames(mod))
+    if (!__hasOwnProp.call(to, key))
+      __defProp(to, key, {
+        get: __accessProp.bind(mod, key),
+        enumerable: true
+      });
+  if (canCache)
+    cache.set(mod, to);
+  return to;
+};
+var __commonJS = (cb, mod) => () => (mod || cb((mod = { exports: {} }).exports, mod), mod.exports);
+
+// src/utils/exec.ts
+import { spawn } from "child_process";
+function run(cmd, args = []) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args);
+    let stdout = "";
+    let stderr = "";
+    child.stdout.on("data", (data) => {
+      stdout += data.toString();
+    });
+    child.stderr.on("data", (data) => {
+      stderr += data.toString();
+    });
+    child.on("close", (code) => {
+      if (code === 0) {
+        resolve(stdout.trim());
+      } else {
+        reject(new Error(stderr || `Process exited with code ${code}`));
+      }
+    });
+  });
+}
+
+// src/exports/vscode.ts
+import fs2 from "fs";
+
+// src/utils/file.ts
+import fs from "fs";
+function exists(path) {
+  try {
+    fs.statSync(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+// src/exports/vscode.ts
+import path from "path";
+import { tap } from "chordsapp";
+var buildVscodeHandler = async function buildVscodeHandler() {
+  const uid = await run("id", ["-u"]);
+  return async function command(cmd) {
+    const tmp = process.env.TMPDIR ?? "/tmp";
+    const dir = path.join(tmp, `vscode-command-server-${uid}`);
+    if (!exists(dir)) {
+      return false;
+    }
+    const requestPath = path.join(dir, "request.json");
+    const responsePath = path.join(dir, "response.json");
+    const payload = JSON.stringify({
+      commandId: cmd,
+      args: []
+    });
+    fs2.writeFileSync(requestPath, payload);
+    fs2.rmSync(responsePath, { force: true });
+    tap("cmd+shift+f17");
+    return true;
+  };
+};
+export {
+  buildVscodeHandler as default
+};
