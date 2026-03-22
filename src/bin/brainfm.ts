7c65a235e194071caadcafad8e7c6d85d7c4cd06
Leon Si <leon@leonsilicon.com>
2026-03-22 01:16:39 -0400

    bun-types

--- src/bin/brainfm.ts
+++ src/bin/brainfm.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env bun
+/// <reference types="bun-types" />
 import CDP from "chrome-remote-interface";
-import getStdin from "get-stdin";
 import jquerySource from "jquery-as-string";
 
 const targets = await CDP.List({ port: 9222 });
 const target = targets.find((t) => t.type === "page" && t.url.includes("index.html"));
 if (!target) throw new Error("No page target found");
-const stdin = await getStdin();
+const stdin = await Bun.stdin.text();
 const client = await CDP({ target });
 const { Runtime } = client;
 await Runtime.enable();
@@ -15,6 +15,7 @@ await Runtime.enable();
 const { result, exceptionDetails } = await Runtime.evaluate({
   expression: `
     ${jquerySource}
+    ;
     ${stdin}
   `,
   awaitPromise: true,
@@ -26,6 +27,6 @@ if (exceptionDetails) {
   console.error("Evaluation failed:", exceptionDetails);
   process.exit(1);
 } else {
-  process.stdout.write(JSON.stringify(result.value));
+  Bun.stdout.write(JSON.stringify(result.value));
   process.exit(0);
 }
