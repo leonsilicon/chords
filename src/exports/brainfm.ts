da1d3bc37885d9ca1f81dba09dd6d6399afdb514
Leon Si <leon@leonsilicon.com>
2026-03-22 01:58:27 -0400

    fix

--- src/exports/brainfm.ts
+++ src/exports/brainfm.ts
@@ -6,14 +6,6 @@ import { join } from "desm";
 export default (function createBrainfmHandler() {
   const brainfmBinpath = join(import.meta.url, "bin/brainfm");
   return async function (code: string) {
-    const subprocess = spawn(brainfmBinpath, {
-      stdout: "inherit",
-      stderr: "inherit",
-      stdin: "pipe",
-    });
-    const nodeSubprocess = await subprocess.nodeChildProcess;
-    nodeSubprocess.stdin?.write(code);
-    nodeSubprocess.stdin?.end();
-    await subprocess;
+    await spawn(brainfmBinpath, { stdin: { string: code } });
   };
 } satisfies BuildHandler);
